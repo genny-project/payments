de5592228d7aabb40fa3fd2e6a198ca6031a005b
matthaywardwebdesign <matt@matthayward.com.au>
2017-12-20 17:32:24 +1100

    Working on endpoints

---
+++ config/roles.js
@@ -0,0 +1,3 @@
+module.exports = {
+  adminRole: process.env.ADMIN_ROLE || 'admin',
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 /* Include dependencies */
 const API = require( '../helpers/api/API' );
 
+/* Include the sub routes */
+require( './users' );
+
 /* Define index routes */
 API.get( '/', ( req, res ) => {
   res.json({ name: 'Payments API' });
