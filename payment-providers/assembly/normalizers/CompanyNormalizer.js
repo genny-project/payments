45815108601625941b1364e7685833771f5219ee
matthaywardwebdesign <matt@matthayward.com.au>
2018-01-17 17:54:58 +1100

    Updated company fields

--- payment-providers/assembly/normalizers/CompanyNormalizer.js
+++ payment-providers/assembly/normalizers/CompanyNormalizer.js
@@ -5,10 +5,13 @@ class CompanyNormalizer {
 
   normalize() {
     const company = this.data;
+
     return {
       legalName: company.legal_name,
       name: company.name,
       id: company.id,
+      taxNumber: company.tax_number,
+      chargesTax: company.charge_tax,
       address: {
         id: company.related.address,
       }
