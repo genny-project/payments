241d40860f360b3dc80482f7caf5bf2cf314ca07
matthaywardwebdesign <matt@matthayward.com.au>
2018-01-17 15:41:36 +1100

    Added more logging

--- helpers/api/API.js
+++ helpers/api/API.js
@@ -5,6 +5,7 @@ const Logger = require( '../logging/Logger' );
 const bodyParser = require( 'body-parser' );
 const cors = require( 'cors' );
 const injectTenant = require( '../../middlewares/auth/injectTenant' );
+const mung = require( 'express-mung' );
 
 /* Create a new express app and listen on the specified port */
 const app = express();
@@ -17,6 +18,18 @@ app.use( bodyParser.json());
 
 /* Add cross origin request support */
 app.use( cors());
+
+/* Add logging of requests and responses if debug mode enabled */
+if ( process.env.DEBUG === 'true' ) {
+  app.use( mung.json(( body, req, res ) => {
+    Logger.info( `${req.method} ${req.url}` );
+    Logger.info( `Request: ${JSON.stringify( req.body, null, 2 )}` );
+    Logger.info( `Response: ${res.statusCode} ${JSON.stringify( body, null, 2 )}` );
+
+    return body;
+  }));
+}
+
 app.httpServer = app.listen( apiConfig.port, () => {
   Logger.info( `API listening on port ${apiConfig.port}` );
 });
