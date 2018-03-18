4bce83658e698e959ce29c08a85b1d1a7a1841ce
matthaywardwebdesign <matt@matthayward.com.au>
2018-03-19 08:25:57 +1100

    Added payment provider support to webhooks

--- routes/webhooks/item/index.js
+++ routes/webhooks/item/index.js
@@ -3,11 +3,11 @@ const WebhooksAPI = require( '../../../helpers/webhooks-api/WebhooksAPI' );
 const Logger = require( '../../../helpers/logging/Logger' );
 const { TenantConfigLoader } = require( '../../../helpers' );
 
-WebhooksAPI.get( '/webhooks/item', ( req, res ) => {
-  res.json({ name: 'Items webhook endpoint' });
+WebhooksAPI.get( '/webhooks/:provider/item', ( req, res ) => {
+  res.json({ name: `Items webhook endpoint for provider ${req.params.provider}` });
 });
 
-WebhooksAPI.post( '/webhooks/item', ( req, res ) => {
+WebhooksAPI.post( '/webhooks/:provider/item', ( req, res ) => {
   /* Get the tenant from the query parameters */
   const { tenantID } = req.query;
 
@@ -17,6 +17,17 @@ WebhooksAPI.post( '/webhooks/item', ( req, res ) => {
   if ( !tenant ) {
     res.status( 400 );
     res.json({ error: 'The tenant ID provided is invalid.' });
+    return;
+  }
+
+  /* Get the payment provider ID */
+  const paymentProviderID = req.params.provider;
+
+  const provider = tenant.getProvider( paymentProviderID );
+
+  if ( provider == null ) {
+    res.status( 400 );
+    res.json({ error: `The tenant with ID ${tenant.getID()} does not have a payment provider with ID ${paymentProviderID}` });
     return;
   }
 
