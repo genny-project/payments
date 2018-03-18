8af22acf0750dbdfa11c5abaeb4f0e5172d45d62
matthaywardwebdesign <matt@matthayward.com.au>
2018-03-19 08:20:54 +1100

    Updated items webhook to be multi tenant

--- routes/webhooks/item/index.js
+++ routes/webhooks/item/index.js
@@ -1,12 +1,25 @@
 /* Include dependencies */
 const WebhooksAPI = require( '../../../helpers/webhooks-api/WebhooksAPI' );
 const Logger = require( '../../../helpers/logging/Logger' );
+const { TenantConfigLoader } = require( '../../../helpers' );
 
 WebhooksAPI.get( '/webhooks/item', ( req, res ) => {
   res.json({ name: 'Items webhook endpoint' });
 });
 
 WebhooksAPI.post( '/webhooks/item', ( req, res ) => {
-  Logger.info( `Incoming item webhook ${JSON.stringify( req.body )}` );
+  /* Get the tenant from the query parameters */
+  const { tenantID } = req.query;
+
+  const tenant = TenantConfigLoader.getTenant( tenantID );
+
+  /* If the tenant doesn't exist throw an error */
+  if ( !tenant ) {
+    res.status( 400 );
+    res.json({ error: 'The tenant ID provided is invalid.' });
+    return;
+  }
+
+  Logger.info( `Incoming item webhook for tenant with ID ${tenant.getID()} ${JSON.stringify( req.body )}` );
   res.json({ success: true });
 });
